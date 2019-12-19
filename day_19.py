f6a0882dee69e3e056c9a5adf0c9e50e3a1b3333
Seth <swidoff@gmail.com>
2019-12-18 23:00:25 -0800

    Day 19. Added comments.

--- day_19.py
+++ day_19.py
@@ -45,16 +45,24 @@ def problem1():
     return sum(1 if c == '#' else 0 for c in beam)
 
 
-def find_square_in_beam(program, dim):
+def find_square_in_beam(program, dim) -> Tuple[int, int]:
+    """
+    Returns the x,y coordinates of the upper left corner of the dim x dim square contained entirely in the
+    beam that has the lowest x value."""
     x = 2
     y = 4
-    coords = defaultdict(list)
+    coords = defaultdict(list)  # For each X axis value, stores the min and max y of the beam.
+
+    # Continue until the lower left corner of the square is in the beam where the upper right corner is the
+    # min y of the last x value.
     while not ((x - dim + 1) in coords and (coords[x][0] + dim - 1) in coords[x - dim + 1]):
+        # Start scanning at the first y of prior x for the start of the beam, since the
+        # beam is pointing "downward" (toward greater y values) the y start will never decrease.
         x += 1
-        print(x)
         while not is_point_in_beam(program, x, y):
             y += 1
 
+        # The beam never shrinks, so skip ahead the previous width of the beam and start scanning from there.
         coords[x].append(y)
         y += max(coords[x - 1][1] - coords[x - 1][0], 1) if len(coords) > 1 else 1
         while is_point_in_beam(program, x, y):
@@ -63,6 +71,7 @@ def find_square_in_beam(program, dim):
         coords[x].append(y - 1)
         y = coords[x][0]
 
+    # Return the upper left corner of the square.
     return x - dim + 1, coords[x][0]
 
 
